138bcb6dc6ab423adaf98bf8939902b8f1f908de
Lightos1 <124387232+Lightos1@users.noreply.github.com>
2026-05-10 00:57:31 +0200

    build.sh: alternatively I should consider not being stupid

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+CORES="$(nproc --all)"
+
 SRC="Source/Atmosphere/stratosphere/loader/"
 DEST="build/stratosphere/loader/"
 mkdir -p "dist/atmosphere/kips/"
@@ -7,8 +9,10 @@ mkdir -p "$DEST"
 
 cp -r "$SRC"/. "$DEST"/
 
+echo "CORES: $CORES"
+
 cd build/stratosphere/loader || exit 1
-make -j8
+make -j$CORES
 hactool -t kip1 out/nintendo_nx_arm64_armv8a/release/loader.kip --uncompress=hoc.kip
 cd ../../../ # exit
 cp build/stratosphere/loader/hoc.kip dist/atmosphere/kips/hoc.kip
@@ -20,5 +24,5 @@ cp -r dist/ ../../
 cd ../../
 
 cd Source/Horizon-OC-Monitor/
-make -j8
+make -j$CORES
 cp Horizon-OC-Monitor.ovl ../../dist/switch/.overlays/Horizon-OC-Monitor.ovl
